eef2a287ed82402b188d9277b49fb1c2db1c25ef
Gregor Richards <hg-yff@gregor.im>
2013-09-04 16:15:11 -0400

    builtin-stages.c converted to .exc. Removed auto-#include of connected header due to problems with priority of _*_SOUCE definitions.
    --HG--
    rename : builtin-stages.c => builtin-stages.exc

--- builtin-stages.h
+++ builtin-stages.h
@@ -1,18 +1,11 @@
-#ifndef BUILTIN_STAGES_H
-#define BUILTIN_STAGES_H
+#ifndef EXC_builtin_stages
+#define EXC_builtin_stages 1
 
 #include "transform.h"
 
-/* @import stage */
-Node *transformImportStage(TransformState *state, Node *node, int isprimary);
-
+ Node *transformImportStage(TransformState *state, Node *node, int isprimary);
 /* @extension stage */
-Node *transformExtensionStage(TransformState *state, Node *node, int isprimary);
-
-/* header stage */
-Node *transformHeaderStage(TransformState *state, Node *node, int isprimary);
-
-/* @raw stage */
-Node *transformRawStage(TransformState *state, Node *node, int isprimary);
-
+ Node *transformExtensionStage(TransformState *state, Node *node, int isprimary);
+ Node *transformHeaderStage(TransformState *state, Node *node, int isprimary);
+ Node *transformRawStage(TransformState *state, Node *node, int isprimary);
 #endif
