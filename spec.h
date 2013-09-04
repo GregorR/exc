9c3205fcf038c5bb29c5d9422910ebc447271890
Gregor Richards <hg-yff@gregor.im>
2013-09-04 17:36:32 -0400

    spec.c -> .exc
    --HG--
    rename : spec.c => spec.exc

--- spec.h
+++ spec.h
@@ -1,38 +1,26 @@
-#ifndef SPEC_H
-#define SPEC_H
+#ifndef EXC_spec
+#define EXC_spec 1
 
 #include "buffer.h"
 
-#ifndef EXC_DEFAULT_SPEC_FILE
-#define EXC_DEFAULT_SPEC_FILE "gcc.excspec"
-#endif
-
-#ifndef EXC_DEFAULT_SPEC_DIR
-#define EXC_DEFAULT_SPEC_DIR "/../share/exc/spec/"
-#endif
-
+/* FIXME: Should this be at the C level or the exc level? */
 BUFFER(charp, char *);
-
-typedef struct SpecCmd_ {
+ typedef struct SpecCmd_ {
     struct Buffer_charp cmd;
     struct Buffer_int repPositions; /* which parameters are replaceable */
     int i, o; /* replacements for input and output file, or -1 for stdin/stdout */
 } SpecCmd;
-
-typedef struct Spec_ {
+ typedef struct Spec_ {
     SpecCmd *cpp, *cc, *ld;
 } Spec;
-
 /* load the default or specified spec file (return allocated) */
-Spec *excLoadSpec(const char *bindir, const char *file);
-
+ Spec *excLoadSpec(const char *bindir, const char *file);
 /* run a spec command with the given replacements */
-struct Buffer_char execSpec(
+ struct Buffer_char execSpec(
     SpecCmd *cmd,
     char *const addlFlags[],
     char *const repNames[],
     char *const repVals[],
     struct Buffer_char input,
     int *status);
-
 #endif
