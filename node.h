f3a2ebe6a1ac60a3dc4dbee94e6b0423c3ed8530
Gregor Richards <hg-yff@gregor.im>
2013-09-04 17:18:53 -0400

    node.c -> .exc
    --HG--
    rename : node.c => node.exc

--- node.h
+++ node.h
@@ -1,25 +1,23 @@
-#ifndef NODE_H
-#define NODE_H
+#ifndef EXC_node
+#define EXC_node 1
 
-#include <sys/types.h>
-
-typedef struct Token_ {
+ typedef struct Token_ {
     int type;
     size_t idx, f, l, c;
     char *pre, *tok;
 } Token;
-
-typedef struct Node_ {
+ typedef struct Node_ {
     struct Node_ *parent;
     int type;
     Token *tok;
     struct Node_ *children[1];
 } Node;
-
-Token *newToken(int type, int dup, char *pre, char *tok);
-void freeToken(Token *);
-
-Node *newNode(Node *parent, int type, Token *tok, size_t len);
-void freeNode(Node *);
-
+/* create a node */
+ Node *newNode(Node *parent, int type, Token *tok, size_t children);
+/* free a node and all the tokens and nodes under it */
+ void freeNode(Node *node);
+/* Create a token. All memory becomes owned by the token. */
+ Token *newToken(int type, int dup, char *pre, char *tok);
+/* free a token */
+ void freeToken(Token *tok);
 #endif
